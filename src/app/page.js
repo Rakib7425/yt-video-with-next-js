241d0d9690a0e7bab90c33b40ada917d899968d6
rakibul Islam <irakibul026@gmail.com>
2024-02-21 01:16:03 +0530

    new file:   .env 	new file:   .eslintrc.json 	new file:   .gitignore 	new file:   README.md 	new file:   jsconfig.json 	new file:   log.txt 	new file:   next.config.mjs 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   src/api_urls.js 	new file:   src/app/layout.js 	new file:   src/app/page.js 	new file:   src/pages/api/download.js 	new file:   src/pages/api/index.js 	new file:   src/pages/api/logger.js 	new file:   src/pages/api/search.js 	new file:   tailwind.config.js 	new file:   vercel.json

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div className='dark:bg-indigo-950 min-h-screen duration-700'>
+      <section className='max-w-[1280px] mx-auto h-full pb-4'>
+        <h1 className='text-2xl font-bold py-8 md:text-yellow-500'>
+          YouTube video Downloader
+        </h1>
+
+      </section>
+    </div>
+  );
+}
